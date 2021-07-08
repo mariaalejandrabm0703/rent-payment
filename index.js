bc1138ffd0e6050e407e630af59f9e2640a9049f
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2021-07-07 19:17:24 -0500

    Configuration.

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8084";
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+  res.status(200).send("WHATABYTE: Food For Devs");
+});
+
+/**
+ * Server Activation
+ */
+ app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
+  
