1fbbf5eb5123839a1a39ed260f2a01401e077478
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2021-07-11 09:48:55 -0500

    Se corrige error de formato de fecha.

--- dist/services/validateFormatDate.js
+++ dist/services/validateFormatDate.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var moment_1 = __importDefault(require("moment"));
 function validateFormatDate(fechaPago) {
     var newDate;
+    var dateFormat = 'DD-MM-YYYY';
     if (fechaPago.includes("/")) {
         newDate = fechaPago.split("/");
         if (newDate.length === 3 &&
             newDate[2].length === 4 &&
             newDate[1].length === 2 &&
-            newDate[0].length === 2 &&
-            moment_1.default(newDate).isValid()) {
+            newDate[0].length === 2) {
             newDate = new Date(newDate[2] + "/" + newDate[1] + "/" + newDate[0]);
-            return newDate;
+            return moment_1.default(moment_1.default(newDate).format(dateFormat), dateFormat, true).isValid() ? newDate : false;
         }
     }
     return false;
--- services/validateFormatDate.ts
+++ services/validateFormatDate.ts
@@ -1,18 +1,19 @@
 import moment from "moment";
 
 export default function validateFormatDate(fechaPago: string) {
+  
   let newDate;
+  let dateFormat = 'DD-MM-YYYY';
   if (fechaPago.includes("/")) {
     newDate = fechaPago.split("/");
     if (
       newDate.length === 3 &&
       newDate[2].length ===4 &&
       newDate[1].length ===2 &&
-      newDate[0].length ===2 &&
-      moment(newDate).isValid()
+      newDate[0].length ===2       
     ) {
       newDate = new Date(newDate[2] + "/" + newDate[1] + "/" + newDate[0]);
-      return newDate;
+      return moment(moment(newDate).format(dateFormat),dateFormat,true).isValid() ? newDate: false
     }
   }
   return false;
